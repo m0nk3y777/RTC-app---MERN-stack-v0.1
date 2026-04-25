f3216b6022f15f8ba3a863566986817cb6d7131e
abdelkader <abdelkader.lakhdar@epitech.eu>
2026-04-25 13:53:21 +0200

    [Setup] Connection a mongoDB via Atlas

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,14 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js"
+import dotenv from "dotenv"
+import { connectDB } from "./libs/db.js"
 
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT
+
 app.use("/api/auth", authRoutes)
-app.listen(5001, () => {
-    console.log("Le serveur écoute sur le port 5001")
+app.listen(PORT, () => {
+    console.log("Le serveur écoute sur le port : " + PORT)
+    connectDB()
 })
